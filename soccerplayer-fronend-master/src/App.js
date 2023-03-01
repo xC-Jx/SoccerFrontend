cd7344f3c496d40c6ef69a5948204f0a2843a7af
xC-Jx <126627121+xC-Jx@users.noreply.github.com>
2023-02-28 18:27:49 -0600

    Add files via upload

---
+++ soccerplayer-fronend-master/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Api from "./components/Api";
+
+//Vamos a crear el una función para consumir nuestra API local
+
+//Vamos a usar useEffect
+
+const App = () => {
+  return (
+    <>
+      {/* Vamos a trar los datos de los jugadores en tarjetas */}
+      <div>App</div>
+      <Api />
+    </>
+  );
+};
+
+export default App;
---
+++ soccerplayer-fronend-master/src/components/Api.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const Api = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      <h1>Componente API</h1>
+    </>
+  );
+};
+
+export default Api;
